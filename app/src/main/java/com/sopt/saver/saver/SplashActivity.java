6094169499891284d00df7868bcd65a451967bed
Bae Hyo-Won <mmhxu95@naver.com>
2017-06-26 16:21:29 +0900

    업데이트

--- app/src/main/java/com/sopt/saver/saver/SplashActivity.java
+++ app/src/main/java/com/sopt/saver/saver/SplashActivity.java
@@ -1,13 +1,23 @@
 package com.sopt.saver.saver;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_splash);
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }

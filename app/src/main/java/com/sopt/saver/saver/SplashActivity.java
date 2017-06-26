4dbaa15cc7a6e2c3a6bd2b57bcc644bf4ff40ba0
gweonyongil <kyi4210@daum.net>
2017-06-26 17:01:37 +0900

    updated 17:01

--- app/src/main/java/com/sopt/saver/saver/SplashActivity.java
+++ app/src/main/java/com/sopt/saver/saver/SplashActivity.java
@@ -2,6 +2,7 @@ package com.sopt.saver.saver;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -9,15 +10,16 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash);
 
-        try {
-            Thread.sleep(3000);
-        }
-        catch (InterruptedException e){
-            e.printStackTrace();
-        }
-        startActivity(new Intent(this, LoginActivity.class));
-        finish();
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 3000);
     }
 }
